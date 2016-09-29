58ae37dbc55c4b280e2d9dca92e2fd2e9f2b87a3
Ben Hu <benjqh@gmail.com>
2016-09-28 22:58:26 -0400

    Update a1q1a.m

--- a1q1a.m
+++ a1q1a.m
@@ -8,13 +8,11 @@ H = hy*hx;
 hx = hx/sum(hx(:));
 hy = hy/sum(hy(:));
 filt = H/sum(H(:));%normalize the filter
-display(filt)
 
 %Question 1 a)
 %img Image 
 %filt Filter assumed n x n where n is odd
 %outImg Convolution of img and filt (output same size as img)
-
 [numRowsFilt, numColsFilt] = size(filt); % Size of input filter
 [numRowsImg, numColsImg] = size(img); % Size of input image
 
@@ -24,23 +22,21 @@ filt=flipud(fliplr(filt));
 % Pad image with k zeroes, k = offset of the filter's origin to an edge 
 filterOffset = (numRowsFilt - 1) / 2;
 paddedImg = padarray(img,[filterOffset filterOffset]);
-paddedImg = rot90(paddedImg);
-paddedImg = flipud(paddedImg);
 
-% Convolve the image
+% Convolution
 for imgRow = 2:numRowsImg+1 %Rows of zero-padded 2:original image
         for imgCol = 2:numColsImg+1 %Columns of zero-padded 2:original image
-            %convolution on pixel paddedImg[imgRow,imgCol] 
+            % Convolution on pixel paddedImg[imgCol,imgRow] 
             areaToFilter = ...
-                (paddedImg(imgRow-filterOffset:imgRow+filterOffset, ...
-                imgCol-filterOffset:imgCol+filterOffset));
+                (paddedImg(imgCol-filterOffset:imgCol+filterOffset, ...
+                imgRow-filterOffset:imgRow+filterOffset));
+            % Sum element-wise mult of areaToFilter and filt
             outImg(imgCol-1,imgRow-1) = sum(sum(areaToFilter.*filt,1),2);
-            %display([imgRow,imgCol])
         end
 end
 
-imgSmooth = conv2(img,filt,'same');
-figure;imagesc(imgSmooth);axis image;colormap gray;
+builtinConv = conv2(img,filt,'same');
+figure;imagesc(builtinConv);axis image;colormap gray;
 
 figure;imagesc(outImg);axis image;colormap gray;
 
