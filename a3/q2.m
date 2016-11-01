4ee27c47a22e8d8d77def67c5292ff58781cf5f9
Ben Hu <benjqh@gmail.com>
2016-11-01 16:42:52 -0400

    Create q2.m

---
+++ a3/q2.m
@@ -0,0 +1,15 @@
+%% Assignment 3 Q2
+
+% Given K r and T, we can compute P, the projective transform 
+% where q = P[x,y,z,1]
+% we take points q1 and q2 and given we have P we can find x,y,z,1 for
+% those points in the real world
+% x will be fixed, we can assume y will be fixed at 0 (no changes in
+% elevation for the railway tracks so the difference between the computed z
+% distances will be the distance between the railway tracks
+% Lecture 10
+
+%% Q2b
+%y = 95 
+%extrinsic
+%tutorial - cameraman.tif example
