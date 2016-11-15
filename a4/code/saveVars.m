265ae19112763ef9ce577f0ffc2a5cf1c3ec051f
Ben-Hu <Benjqh@gmail.com>
2016-11-14 21:21:40 -0500

    saveVars.m L1-3,R1-3

---
+++ a4/code/saveVars.m
@@ -0,0 +1,63 @@
+R1.cmCar = cm1cR
+R1.cmPerson = cm1pR
+R1.cmBike = cm1bR
+R1.img = img1Right
+R1.carRes = img1RightCarRes
+R1.personRes = img1RightPersonRes
+R1.bikeRes = img1RightBicycleRes
+
+save('./R1.mat', '-struct', 'R1')
+
+R2.cmCar = cm2cR
+R2.cmPerson = cm2pR
+R2.cmBike = cm2bR
+R2.img = img2Right
+R2.carRes = img2RightCarRes
+R2.personRes = img2RightPersonRes
+R2.bikeRes = img2RightBicycleRes
+
+save('./R2.mat', '-struct', 'R2')
+
+R3.cmCar = cm3cR
+R3.cmPerson = cm3pR
+R3.cmBike = cm3bR
+R3.img = img3Right
+R3.carRes = img3RightCarRes
+R3.personRes = img3RightPersonRes
+R3.bikeRes = img3RightBicycleRes
+
+save('./R3.mat', '-struct', 'R3')
+
+
+L1.cmCar = cm1cL
+L1.cmPerson = cm1pL
+L1.cmBike = cm1bL
+L1.img = img1Left
+L1.carRes = img1LeftCarRes
+L1.personRes = img1LeftPersonRes
+L1.bikeRes = img1LeftBicycleRes
+
+save('./L1.mat', '-struct', 'L1')
+
+L2.cmCar = cm2cL
+L2.cmPerson = cm2pL
+L2.cmBike = cm2bL
+L2.img = img2Left
+L2.carRes = img2LeftCarRes
+L2.personRes = img2LeftPersonRes
+L2.bikeRes = img2LeftBicycleRes
+
+save('./L2.mat', '-struct', 'L2')
+
+L3.cmCar = cm3cL
+L3.cmPerson = cm3pL
+L3.cmBike = cm3bL
+L3.img = img3Left
+L3.carRes = img3LeftCarRes
+L3.personRes = img3LeftPersonRes
+L3.bikeRes = img3LeftBicycleRes
+
+save('./L3.mat', '-struct', 'L3')
+
+
+
