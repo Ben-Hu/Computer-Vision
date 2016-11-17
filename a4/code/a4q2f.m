d550289557d41cf350a3a819c64bb8f7037c6bd3
Ben-Hu <Benjqh@gmail.com>
2016-11-17 13:04:42 -0500

    generate a textual description of the scene

--- a4/code/a4q2f.m
+++ a4/code/a4q2f.m
@@ -1,50 +1,106 @@
 %%Assignment 4 Q2f
-
-% Create a textual description of the scene. 
-% Convey more important facts before the less important ones. 
-% Think what you, as a driver, would like to know first.
-% Think of the camera centre as the driver.
-% 
-% In your description, generate at least a sentence about 
-% how many cars, how many people and cyclists are in the scene. 
-% 
-% Generate also a sentence that tells the driver 
-% which object is the closest to him and where it is.
-% 
-% For example, let’s say you have an object with location (X,Y,Z) 
-% in 3D and label = car and you want to generate a sentence about where it is. 
-% 
-% You could do something like:
-% d = norm([X, Y, Z]); % this is the distance of object to driver 
-% if X ≥ 0, txt = “to your right”; 
-% else txt = “to your left”; 
-% end; 
-% fprintf(“There is a %s %0.1f meters %s \n”, label, X, txt);
-% fprintf(“It is %0.1 meters away from you \n”, d);
-
-
-% Total object counts : Histogram
-
 % Descriptive output:
 % Sequence order:
 % Cars > Bikes > Persons 
+% or by object distance regardless of type
 
-% For each object:
-% Use center of mass for:
-% Left/Right of Camera Center (viewpoint_xlim/2)
-% Distance from Camera 
+L1 = load('L1.mat');
+L2 = load('L2.mat');
+L3 = load('L3.mat');
 
-% How to remove false positives - should have done better NMS at start -
-% adjusting for object type and being more strict about outliers
-% Still, scores are inconsistent across objects
-% Taking a look at the data use the following relaxations on detection
-% score
+R1 = load('R1.mat');
+R2 = load('R2.mat');
+R3 = load('R3.mat');
 
-% Car (Score > 0)
-% Bike (Score >= -0.5)
-% Person (Score >= -0.5)
+[L1a,L1b] = getDescription(L1);
+[L2a,L2b] = getDescription(L2);
+[L3a,L3b] = getDescription(L3);
 
-% Camera center = size(img,2)/2
+[R1a,R1b] = getDescription(R1);
+[R2a,R2b] = getDescription(R2);
+[R3a,R3b] = getDescription(R3);
+
+function [desc,desc2]=getDescription(scene)
+    desc = string('');
+    
+    cars = scene.resCar;
+    people = scene.resPerson;
+    bikes = scene.resBike;
+    
+    %thresholding out probable false detections
+    cThresh = -0.5;
+    pThresh = -0.48;
+    bThresh = -0.95;%-0.5;
+    cars = cars(cars(:,6)>cThresh,:);
+    people = people(people(:,6)>=pThresh,:);
+    bikes = bikes(bikes(:,6)>=bThresh,:);
+    
+    centerLine = size(scene.img,2)/2;
+    
+    %% Form the description of the scene - by ordered by object type
+    numCars = size(cars,1);
+    numPeople = size(people,1);
+    numBikes = size(bikes,1);
+    desc = desc + sprintf('There are %d cars, %d persons, and %d bikes ahead\n',numCars, numPeople, numBikes);
+    desc2 = desc;
+    
+    byDist = [];
+    for i=1:numCars
+        dist = round(scene.cmCar(i,3)); %depth for this object
+        objX = scene.cmCar(i,1); %x position of this object
+        if objX > centerLine
+            direction = 'right';
+        else
+            direction = 'left';
+        end
+        desc = desc + sprintf('Car %d is %d m away to your %s\n',i,dist,direction);
+        byDist = cat(1,byDist,[1,dist,objX]);
+    end
+    
+    for i=1:numPeople
+        dist = round(scene.cmPerson(i,3)); %depth for this object
+        objX = scene.cmPerson(i,1); %x position of this object
+        if objX > centerLine
+            direction = 'right';
+        else
+            direction = 'left';
+        end
+        desc = desc + sprintf('Person %d is %d m away to your %s\n',i,dist,direction);
+        byDist = cat(1,byDist,[2,dist,objX]);
+    end
+    
+    for i=1:numBikes
+        dist = round(scene.cmBike(i,3)); %depth for this object
+        objX = scene.cmBike(i,1); %x position of this object
+        if objX > centerLine
+            direction = 'right';
+        else
+            direction = 'left';
+        end
+        desc = desc + sprintf('Bike %d is %d m away to your %s\n',i,dist,direction);
+        byDist = cat(1,byDist,[3,dist,objX]);
+    end
+    
+    %% Second description type, ordered by distance instead of type
+    byDist = sortrows(byDist,2);
+    for i=1:size(byDist,1)
+        if byDist(i,1) == 1
+            type = 'Car';
+        elseif byDist(i,1) == 2
+            type = 'Person';
+        else
+            type = 'Bike';
+        end
+        
+        if byDist(i,3) > centerLine
+            direction = 'right';
+        else
+            direction = 'left';
+        end
+        
+        desc2 = desc2 + sprintf('There is a %s %d m to your %s\n', type, byDist(i,2), direction);
+    end  
+end
 
 
 
